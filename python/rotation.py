746585ecdd1aef4b9491a5453c6e4123fe837e3b
Saul <saulc@users.noreply.github.com>
2020-02-08 14:09:38 -0800

    doing math
    ex 3

--- python/rotation.py
+++ python/rotation.py
@@ -2,24 +2,38 @@ import math
 import numpy as np
 
 
-print (math.pi)
+def deg(rad):
+    return rad/pi
 
-# https://www.programiz.com/python-programming/matrix
-v = np.array([ [5], [30], [10] ])
-A = np.array([[2, 4], [5, -6]])
-B = np.array([[9, -3], [3, 6]])
-C = A + B      # element wise addition
-print(A)
-print(B)
-print("C = A + B")
-print(C)
-print("C = A dot B")
-C = A.dot(B)
-print(C)
-
-# x = np.array([ [sin(30)] ], [30], [10] ])
+def rad(deg):
+    return math.pi * deg/180
 
 
+def demo():
+    print (math.pi)
+    # https://www.programiz.com/python-programming/matrix
+    v = np.array([ [5], [30], [10] ])
+    A = np.array([[2, 4], [5, -6]])
+    B = np.array([[9, -3], [3, 6]])
+    C = A + B      # element wise addition
+    print(A)
+    print(B)
+    print("C = A + B")
+    print(C)
+    print("C = A dot B")
+    C = A.dot(B)
+    print(C)
+
+def ex3():
+    v = np.array([ [5], [30], [10] ])
+
+    r1 = np.array([ [math.cos(rad(30)), -math.sin(rad(30)), 0] ,
+                [math.sin(rad(30)), math.cos(rad(30)), 0],
+                [0, 0, 1] ])
+    print(r1)
+
+
+ex3()
 
 
 # A = [[1, 4, 5, 12],
