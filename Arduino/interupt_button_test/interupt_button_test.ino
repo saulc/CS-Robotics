322be8fc382e868eb90256f7df9b38798a53ef1c
Saul <saulc@users.noreply.github.com>
2020-02-24 02:22:25 -0800

    comments..
    ...

--- Arduino/interupt_button_test/interupt_button_test.ino
+++ Arduino/interupt_button_test/interupt_button_test.ino
@@ -1,23 +1,15 @@
 /*
-  Input Pull-up Serial
-
-  This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a digital
-  input on pin 2 and prints the results to the Serial Monitor.
-
+  2/24/20
+  Saul C.
+  
+  Input Pull-up Serial example  modified for interupts,
+  multiple states cycled by a push button.
+  with basic timed debounce.
+  
   The circuit:
   - momentary switch attached from pin 2 to ground
   - built-in LED on pin 13
 
-  Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
-  20K-ohm resistor is pulled to 5V. This configuration causes the input to read
-  HIGH when the switch is open, and LOW when it is closed.
-
-  created 14 Mar 2012
-  by Scott Fitzgerald
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/InputPullupSerial
 */
 
 int s = 0;
